d0c5f35f6e834a1a9d721f5181d1067f242c11b4
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-10-17 23:50:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// var currentDate = new Date();
+// document.write(currentDate.getHours() + ":" + )
+// document.write(currentDate.getMinutes() + ":")
+// var currentSec = currentDate.getSeconds();
+// // document.write(currentSec);
+// if (currentSec < 10) {
+//     document.write("0" + currentSec);
+// } else {
+//     document.write(currentSec);
+// }
